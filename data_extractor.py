2d7f26bf63dde2f5ede9e459f85135ed2e6bf302
cperry <cmallaryp@gmail.com>
2024-07-17 07:59:21 -0400

    SQL code updated

--- data_extractor.py
+++ data_extractor.py
@@ -1,3 +1,4 @@
+import PIL.Image
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.io import imread, imshow
@@ -6,34 +7,20 @@ import pytesseract
 import PIL
 from os.path import exists
 
+plt.switch_backend('TkAgg')
 
+# global transform variables
+x_transform = 780
+y_middle_transform = 675
+y_bottom_transform = 1346
+text_crop_species = (241, 555, 740, 690)
+text_crop_bools = (610, 420, 700, 550)
+x_transform_tuple = (x_transform, 0, x_transform, 0)
+y_middle_transform_tuple = (0, y_middle_transform, 0, y_middle_transform)
+y_bottom_transform_tuple = (0, y_bottom_transform, 0, y_bottom_transform)
 
-def process_images():
-    phylas = {'images/gymnosperms_', 'images/pteridophytes_', 'images/angiosperm_monocots_', 'images/angiosperm_dicots_'}
-    for phyla in phylas:
-        for i in range(5000):
-            while exists(phyla + str(i) + '.jpeg') is True:
-                path = phyla + str(i) + '.jpeg'
-                current_image = imread(path, as_gray = True)
-
-                break
-def main():
-    conn = psy.connect('dbname=postgres user=postgres password=password host=host.docker.internal')
-    conn.autocommit = True
-
-    cur = conn.cursor()
-    cur.execute("SET search_path TO ar_plants;")
-
-    plt.switch_backend('TkAgg')
+def process_images(cur, path, phyla):
     
-    process_images()
-
-    image = imread('images/gymnosperms_1.jpeg', as_gray = True)
-    # pixel amounts to transfer between state maps
-    x_transform = 780
-    y_middle_transform = 675
-    y_bottom_transform = 1346
-
     # y is before x when indexing into the image
     counties = [
         {"name": "arkansas",
@@ -338,11 +325,82 @@ def main():
         },
     ]
 
-
     # quadrants are numbered right to left, top to bottom from 0 - 5
     transform_x = 0
     transform_y = 0
     for quadrant in range(6):
+        image = imread(path, as_gray = True)
+        img = PIL.Image.open(path)
+        
+        # extract text data based on quadrant
+        if quadrant == 0:
+            quadrant0 = img.crop(text_crop_species)
+            quadrant0_bool = img.crop(text_crop_bools)
+            q0 = np.asarray(quadrant0)
+            q_bool0 = np.asarray(quadrant0_bool)
+            raw_text0 = pytesseract.image_to_string(q0)
+            lines_text0 = raw_text0.splitlines()       
+        elif quadrant == 1:
+            quadrant1 = img.crop(tuple(np.add(text_crop_species, x_transform_tuple)))
+            quadrant1_bool = img.crop(tuple(np.add(text_crop_bools, x_transform_tuple)))
+            q_bool1 = np.asarray(quadrant1_bool)
+            q1 = np.asarray(quadrant1)
+            raw_text1 = pytesseract.image_to_string(q1)
+            lines_text1 = raw_text1.splitlines()
+        elif quadrant == 2:
+            quadrant2 = img.crop(tuple(np.add(text_crop_species, y_middle_transform_tuple)))
+            quadrant2_bool = img.crop(tuple(np.add(text_crop_bools, y_middle_transform_tuple)))
+            q_bool2 = np.asarray(quadrant2_bool)
+            q2 = np.asarray(quadrant2)
+            raw_text2 = pytesseract.image_to_string(q2)
+            lines_text2 = raw_text2.splitlines()
+        elif quadrant == 3:
+            quadrant3 = img.crop(tuple(np.add((tuple(np.add(text_crop_species, y_middle_transform_tuple))), x_transform_tuple)))
+            quadrant3_bool = img.crop(tuple(np.add((tuple(np.add(text_crop_bools, y_middle_transform_tuple))), x_transform_tuple)))
+            q3 = np.asarray(quadrant3)
+            q_bool3 = np.asarray(quadrant3_bool)
+            raw_text3 = pytesseract.image_to_string(q3)
+            lines_text3 = raw_text3.splitlines()   
+        elif quadrant == 4:
+            quadrant4 = img.crop(tuple(np.add(text_crop_species, y_bottom_transform_tuple)))
+            quadrant4_bool = img.crop(tuple(np.add(text_crop_bools, y_bottom_transform_tuple)))
+            q4 = np.asarray(quadrant4)
+            q_bool4 = np.asarray(quadrant4_bool)
+            raw_text4 = pytesseract.image_to_string(q4)
+            lines_text4 = raw_text4.splitlines()
+        elif quadrant == 5:
+            quadrant5 = img.crop(tuple(np.add((tuple(np.add(text_crop_species, y_bottom_transform_tuple))), x_transform_tuple)))
+            quadrant5_bool = img.crop(tuple(np.add((tuple(np.add(text_crop_bools, y_bottom_transform_tuple))), x_transform_tuple)))
+            q5 = np.asarray(quadrant5)
+            q_bool5 = np.asarray(quadrant5_bool)
+            raw_text5 = pytesseract.image_to_string(q5)
+            lines_text5 = raw_text5.splitlines()
+        else:
+            return 1
+        
+        if eval(("lines_text" + str(quadrant)))[0].isalnum() == False:
+            continue
+        phyla = str(phyla.removeprefix("images/").removesuffix("_"))
+        print(phyla)
+        cur.execute("SELECT phyla_id FROM phyla WHERE polyphylactic_group = %s", (phyla,))
+        phyla_id = cur.fetchone()[0]
+        print(phyla_id)
+        family = eval(("lines_text" + str(quadrant)))[0]
+        genera = eval(("lines_text" + str(quadrant)))[1].split(" ")[0]
+        species = eval(("lines_text" + str(quadrant)))[1]+ " " + (eval(("lines_text" + str(quadrant)))[2].split("  ")[0])
+        common = eval(("lines_text" + str(quadrant)))[3]
+
+        cur.execute("SELECT family_id FROM family WHERE family = %s", (family,))
+        family_id_test = cur.fetchone()[0]
+        if family_id_test == False:    
+            cur.execute("INSERT INTO family VALUES (DEFAULT, %s, %s)", (family, phyla_id))
+        else:
+            continue
+        cur.execute("SELECT family_id FROM family WHERE family = %s", (family,))
+        family_id = cur.fetchone()[0]
+        cur.execute("INSERT INTO genera VALUES (DEFAULT, %s, %s, %s)", (genera, family_id, phyla_id))
+        print(family, genera, species, common)
+
         # x transform conditions
         if quadrant % 2 != 0:
             transform_x = x_transform
@@ -359,46 +417,31 @@ def main():
         for county in counties:
             county_occurance = image[county["y"] + transform_y][county["x"] + transform_x]
             if county_occurance < 0.5:
-                species_id = cur.execute("SELECT species_id FROM species WHERE scientific_name = %s;", (VARIABLE_TODO,))
-                county_id = cur.execute("SELECT county_id FROM counties WHERE county_name = %s;", (county["name"],))
-                cur.execute("INSERT INTO county_occurance VALUES (%s, %s);", (species_id, county_id,))
-                cur.close()
-        
+                # species_id = cur.execute("SELECT species_id FROM species WHERE scientific_name = %s;", (VARIABLE_TODO,))
+                # county_id = cur.execute("SELECT county_id FROM counties WHERE county_name = %s;", (county["name"],))
+                # cur.execute("INSERT INTO county_occurance VALUES (%s, %s);", (species_id, county_id,))
+                continue
+    return
+                
+def main():
+    conn = psy.connect('dbname=postgres user=postgres password=password host=host.docker.internal')
+    conn.autocommit = True
 
+    cur = conn.cursor()
+    cur.execute("SET search_path TO ar_plants;")
 
-    text_crop_species = (241, 555, 740, 690)
-    text_crop_bools = (610, 420, 700, 550)
-    x_transform_tuple = (x_transform, 0, x_transform, 0)
-    y_middle_transform_tuple = (0, y_middle_transform, 0, y_middle_transform)
-    y_bottom_transform_tuple = (0, y_bottom_transform, 0, y_bottom_transform)
-
-    img = PIL.Image.open("images/gymnosperms_2.jpeg")
-
-    # crop image to isolate family, genera, and species text
-    quadrant0 = img.crop(text_crop_species)
-    quadrant1 = img.crop(tuple(np.add(text_crop_species, x_transform_tuple)))
-    quadrant2 = img.crop(tuple(np.add(text_crop_species, y_middle_transform_tuple)))
-    quadrant3 = img.crop(tuple(np.add((tuple(np.add(text_crop_species, y_middle_transform_tuple))), x_transform_tuple)))
-    quadrant4 = img.crop(tuple(np.add(text_crop_species, y_bottom_transform_tuple)))
-    quadrant5 = img.crop(tuple(np.add((tuple(np.add(text_crop_species, y_bottom_transform_tuple))), x_transform_tuple)))
-
-    # turn images into NDarray's
-    q0 = np.asarray(quadrant0)
-    q1 = np.asarray(quadrant1)
-    q2 = np.asarray(quadrant2)
-    q3 = np.asarray(quadrant3)
-    q4 = np.asarray(quadrant4)
-    q5 = np.asarray(quadrant5)
-
-    raw_text0 = pytesseract.image_to_string(q0)
-
-    lines_text0 = raw_text0.splitlines()
-    family = lines_text0[0]
-    genera = (lines_text0[1].split(" ")[0])
-    species = lines_text0[1]
-    common = lines_text0[3]
-
-
+    # gather county data
+    
+    phylas = {'images/gymnosperms_', 'images/pteridophytes_', 'images/angiosperm_monocots_', 'images/angiosperm_dicots_'}
+    for phyla in phylas:
+        for i in range(5000):
+            if exists(phyla + str(i) + '.jpeg') is True:
+                path = phyla + str(i) + '.jpeg'
+                process_images(cur, path, phyla)
+                # upload_to_database(data)
+                continue
+            else:
+                continue
 
 
     # print(genera)
@@ -409,85 +452,5 @@ def main():
     # imshow(q1)
     # plt.show()
 
-
-
-
-    counties1 = {
-        "arkansas": image[381][556],
-        "ashley":  image[526][516],
-        "baxter": image[122][454],
-        "benton":image[122][256],
-        "boone": image[128][377],
-        "bradley": image[490][473],
-        "calhoun": image[480][438],
-        "carroll": image[122][331],
-        "chicot": image[514][565],
-        "clark": image[418][368],
-        "clay": image[113][651],
-        "cleburne": image[225][484],
-        "cleveland": image[436][474],
-        "columbia": image[525][362],
-        "conway": image[261][418],
-        "craighead": image[186][633],
-        "crawford": image[219][254],
-        "crittenden": image[255][664],
-        "cross": image[255][620],
-        "dallas": image[428][421],
-        "desha": image[447][566],
-        "drew": image[475][521],
-        "faulkner": image[278][455],
-        "franklin": image[225][296],
-        "fulton": image[116][505],
-        "garland": image[351][368],
-        "grant": image[388][443],
-        "greene": image[150][636],
-        "hempstead": image[456][316],
-        "hotspring": image[386][399],
-        "howard": image[414][285],
-        "independence": image[202][532],
-        "izard": image[155][499],
-        "jackson": image[219][574],
-        "jefferson": image[383][494],
-        "johnson": image[222][335],
-        "lafayette": image[525][321],
-        "lawrence": image[163][583],
-        "lee": image[322][619],
-        "lincoln": image[430][519],
-        "littleriver": image[462][249],
-        "logan": image[263][312],
-        "lonoke": image[325][504],
-        "madison": image[170][310],
-        "marion": image[134][415],
-        "miller": image[509][290],
-        "mississippi": image[189][686],
-        "monroe": image[333][574],
-        "montgomery": image[355][316],
-        "nevada": image[468][354],
-        "newton": image[128][377],
-        "ouachita": image[475][393],
-        "perry": image[300][395],
-        "phillips": image[358][616],
-        "pike": image[402][316],
-        "poinsett": image[219][626],
-        "polk": image[358][258],
-        "pope": image[241][379],
-        "prairie": image[316][538],
-        "pulaski": image[325][460],
-        "randolph": image[120][591],
-        "saintfrancis": image[288][619],
-        "saline": image[341][418],
-        "scott": image[313][276],
-        "searcy": image[178][416],
-        "sebastian": image[265][251],
-        "sevier": image[420][251],
-        "sharp": image[152][538],
-        "stone": image[185][465],
-        "union": image[531][426],
-        "vanburen": image[219][438],
-        "washington": image[170][260],
-        "white": image[264][516],
-        "woodruff": image[269][572],
-        "yell": image[294][338]
-        }
     
 main()
